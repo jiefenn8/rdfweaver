cdc004de0e1fb00a52cfa76abdebcba4d675598c
Javen Liu <jiefenn8@outlook.com>
2020-04-23 01:58:13 +1200

    Add mapping config file command
    - Add mapping config file command
    - Add R2RML config loader

---
+++ src/main/java/App.java
@@ -0,0 +1,34 @@
+import com.github.jiefenn8.graphloom.rdf.parser.R2RMLBuilder;
+import com.github.jiefenn8.graphloom.rdf.r2rml.R2RMLMap;
+import options.R2RMLOption;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.ParseResult;
+import picocli.CommandLine.Spec;
+
+@Command(name = "rdfweaver",
+        mixinStandardHelpOptions = true,
+        synopsisSubcommandLabel = "COMMAND",
+        subcommands = {R2RMLOption.class},
+        version = "0.1.0")
+public class App implements Runnable {
+
+    @Spec private CommandSpec spec;
+    private R2RMLBuilder builder;
+    private R2RMLMap r2rmlMap;
+
+    public static void main(String... args) {
+    }
+
+    @Override
+    public void run() {
+        ParseResult parseResult = spec.commandLine().getParseResult();
+
+        if (parseResult.hasSubcommand()) {
+            for (ParseResult pr : parseResult.subcommands()) {
+                pr.commandSpec().commandLine().getCommandName();
+            }
+        }
+    }
+
+}
---
+++ src/main/java/options/R2RMLOption.java
@@ -0,0 +1,53 @@
+package options;
+
+import com.github.jiefenn8.graphloom.rdf.parser.R2RMLBuilder;
+import com.github.jiefenn8.graphloom.rdf.r2rml.R2RMLMap;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jena.n3.turtle.TurtleParseException;
+import org.apache.jena.shared.NotFoundException;
+import picocli.CommandLine;
+import picocli.CommandLine.Model.CommandSpec;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+import static picocli.CommandLine.*;
+
+@Command(name = "r2rml",
+        exitCodeOnInvalidInput = 12,
+        exitCodeOnExecutionException = 14,
+        description = "Load a R2RML file to configure the mapping of data to RDF.")
+public class R2RMLOption implements Callable<R2RMLMap> {
+
+    private final R2RMLBuilder builder;
+    @Option(names = {"-f", "--file"}, required = true, description = "R2RML config file.")
+    private final File r2rmlFile = new File(StringUtils.EMPTY);
+    @Spec private CommandSpec spec;
+
+    public R2RMLOption(R2RMLBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Override
+    public R2RMLMap call() {
+        CommandLine cmd = spec.commandLine();
+        String path = r2rmlFile.getPath();
+        try {
+            if (!r2rmlFile.isFile()) {
+                String message = String.format("%s not found.", path);
+                throw new NotFoundException(message);
+            }
+
+            return builder.parse(r2rmlFile.getPath());
+        } catch (NotFoundException e) {
+            String message = String.format("R2RML file: '%s' is not valid filename or path.", path);
+            cmd.getErr().println(message);
+            throw new ExecutionException(cmd, message, e);
+
+        } catch (TurtleParseException e) {
+            String message = String.format("R2RML file: '%s' does not contain a valid R2RML map.", path);
+            cmd.getErr().println(message);
+            throw new ExecutionException(cmd, message, e);
+        }
+    }
+}
