b0237a6dcf272927bb3e5d9c8c1dc00517156f58
jiefenn8 <jiefenn8@outlook.com>
2020-11-12 23:43:50 +1300

    Add RDB to RDF mapper implementation

---
+++ src/integration-test/java/com/github/jiefenn8/rdfweaver/integrationtest/options/AppTest.java
@@ -0,0 +1,48 @@
+package com.github.jiefenn8.rdfweaver.integrationtest.options;
+
+import com.github.jiefenn8.rdfweaver.App;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Integration test class for {@code App}.
+ */
+public class AppTest {
+    
+    private Path expectedOutput;
+
+    @Before
+    public void setUp() {
+        expectedOutput = Paths.get("output/rdfOutput.nt").toAbsolutePath();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Files.deleteIfExists(expectedOutput);
+    }
+
+    @Test
+    public void GivenRequiredParams_WhenExecute_ThenGenerateRDFFile() {
+        String driver = "--driver=MSSQL";
+        String host = "--host=localhost";
+        String port = "--port=1433";
+        String database = "--database=testDb";
+        String user = "--user=sa";
+        String pass = "--pass=YourStrong@Passw0rd";
+        String r2rmlFile = getClass().getResource("/r2rml/valid_r2rml.ttl").getPath();
+        String r2rml = "--file=" + r2rmlFile;
+        String[] args = new String[]{"server", driver, database, host, port, user, pass, "r2rml", r2rml, "output"};
+
+        App.main(args);
+        boolean result = Files.exists(expectedOutput);
+        assertThat(result, is(true));
+    }
+}
--- src/main/java/com/github/jiefenn8/rdfweaver/App.java
+++ src/main/java/com/github/jiefenn8/rdfweaver/App.java
@@ -1,29 +1,149 @@
 package com.github.jiefenn8.rdfweaver;
 
-import com.github.jiefenn8.graphloom.rdf.parser.R2RMLBuilder;
-import com.github.jiefenn8.graphloom.rdf.r2rml.R2RMLMap;
-import com.github.jiefenn8.rdfweaver.options.R2RMLOption;
+import com.github.jiefenn8.graphloom.api.ConfigMaps;
+import com.github.jiefenn8.graphloom.api.InputSource;
+import com.github.jiefenn8.graphloom.rdf.RDFMapper;
 import com.github.jiefenn8.rdfweaver.options.ServerOption;
-import picocli.CommandLine.Command;
+import com.github.jiefenn8.rdfweaver.output.RDFFile;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import picocli.CommandLine;
+import picocli.CommandLine.*;
 import picocli.CommandLine.Model.CommandSpec;
-import picocli.CommandLine.Spec;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Command(name = "rdfweaver",
         mixinStandardHelpOptions = true,
         synopsisSubcommandLabel = "COMMAND",
-        subcommands = {R2RMLOption.class, ServerOption.class},
         version = "0.1.0")
 public class App implements Runnable {
 
+    private static final Logger LOGGER = LogManager.getLogger(App.class);
+    private final RDFMapper rdfMapper;
+    private final ServerOption serverOption;
     @Spec private CommandSpec spec;
-    private R2RMLBuilder builder;
-    private R2RMLMap r2rmlMap;
+    private CommandLine cmd;
+    private InputSource inputSource;
+    private ConfigMaps configMaps;
+    private RDFFile rdfFile;
 
-    public static int main(String... args) {
-        return 0;
+    /**
+     * Constructs an App instance with default {@link RDFMapper}.
+     */
+    public App() {
+        rdfMapper = new RDFMapper();
+        serverOption = new ServerOption();
     }
 
+    /**
+     * Constructs an App instance with specified {@link RDFMapper}.
+     *
+     * @param rdfMapper the mapper to use
+     */
+    public App(@NonNull RDFMapper rdfMapper, @NonNull ServerOption serverOption) {
+        this.rdfMapper = rdfMapper;
+        this.serverOption = serverOption;
+    }
+
+    /**
+     * Main program execution entry.
+     *
+     * @param args the arguments given to app
+     */
+    public static void main(@NonNull String... args) {
+        LOGGER.trace("Entering application.");
+        App app = new App();
+        int code = app.start(new CommandLine(app), args);
+        LOGGER.trace("Exiting application with code " + code + ".");
+        LOGGER.traceExit();
+    }
+
+    /**
+     * Checks that the required subcommands is given as argument. Throws an
+     * exception if the requirements are not met.
+     */
     @Override
     public void run() {
+        cmd = spec.commandLine();
+        if (!hasRequiredCommands()) {
+            throw new ParameterException(cmd, "server, r2rml, output subcommands required.");
+        }
+    }
+
+    protected int start(@NonNull CommandLine cmd, @NonNull String... args){
+        cmd.addSubcommand(serverOption);
+        cmd.setExecutionStrategy(new RunAll());
+        return processResults(cmd.execute(args));
+    }
+
+    /**
+     * Returns true when required commands have been given to this application,
+     * otherwise return false.
+     *
+     * @return true if required commands is given, otherwise false
+     */
+    private boolean hasRequiredCommands(){
+        List<String> commands = cmd.getParseResult().originalArgs();
+        List<String> required = Arrays.asList("server", "r2rml", "output");
+        return commands.containsAll(required);
+    }
+
+    /**
+     * Takes the execution code from command line execution and checks if all
+     * the commands have be executed successfully. If so, process relevant
+     * results to map input data to a RDF model for output using
+     * {@link RDFMapper#mapToGraph(InputSource, ConfigMaps)}.
+     *
+     * @param code the execution result from application
+     * @return the execution code of the result processing
+     */
+    private int processResults(int code) {
+        if(code != 0 ){
+            return code;
+        }
+
+        cmd.getOut().println("Starting RDF mapping process.");
+        collectAllSubResults(cmd).forEach((obj) -> { if (obj instanceof InputSource) inputSource = ((InputSource) obj);
+            if (obj instanceof ConfigMaps) configMaps = ((ConfigMaps) obj);
+            if (obj instanceof RDFFile) rdfFile = ((RDFFile) obj);
+        });
+
+        try (FileOutputStream fos = new FileOutputStream(rdfFile)) {
+            Model result = rdfMapper.mapToGraph(inputSource, configMaps);
+            RDFDataMgr.write(fos, result, rdfFile.getFormat());
+            cmd.getOut().println("RDF mapping complete. Output: " + rdfFile);
+        } catch (IOException ex) {
+            ex.printStackTrace(cmd.getErr());
+        }
+
+        return spec.exitCodeOnSuccess();
+    }
+
+    /**
+     * Recursively collect all results from top commands and any nested
+     * subcommands that has been executed.
+     *
+     * @param cmd the root command to start collection from
+     * @return list of object containing all the command results
+     */
+    private List<Object> collectAllSubResults(@NonNull CommandLine cmd) {
+        List<Object> resultList = new ArrayList<>();
+        ParseResult parseResult = cmd.getParseResult();
+        if (parseResult.hasSubcommand()) {
+            CommandLine subCommandLine = parseResult.subcommand()
+                    .commandSpec()
+                    .commandLine();
+            resultList.add(subCommandLine.getExecutionResult());
+            resultList.addAll(collectAllSubResults(subCommandLine));
+        }
+        return resultList;
     }
 }
