19018cd1af2f1c1ccc48c1ee74cbe354cdfa5d97
jiefenn8 <jiefenn8@outlook.com>
2020-11-20 18:11:09 +1300

    Fix database setup script
    Fix database setup script to address possible connection issue
    
    [skip ci]

--- script/docker_mssql_setup_script.sh
+++ script/docker_mssql_setup_script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #Script to setup and populate mssql database with a docker image
 docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=YourStrong@Passw0rd' -p 1433:1433 --name sql1 -v $TRAVIS_BUILD_DIR/script:/script -d mcr.microsoft.com/mssql/server:2019-latest
-docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourStrong@Passw0rd -i /script/sqlserver_test_db.sql
+docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U sa -P YourStrong@Passw0rd -i /script/sqlserver_test_db.sql
  
