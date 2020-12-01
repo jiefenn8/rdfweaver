526a35c754c79517b91368969cad2148b7a9b902
jiefenn8 <jiefenn8@outlook.com>
2020-12-02 01:20:38 +1300

    Update output options into relevant argument groups
    Update output options into relevant argument groups for future implementation of other output types with their own exclusive options.

--- src/main/java/com/github/jiefenn8/rdfweaver/output/OutputOption.java
+++ src/main/java/com/github/jiefenn8/rdfweaver/output/OutputOption.java
@@ -4,14 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.ExecutionException;
+import picocli.CommandLine.*;
 import picocli.CommandLine.Model.CommandSpec;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Spec;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
@@ -27,21 +25,11 @@ import java.util.concurrent.Callable;
 public class OutputOption implements Callable<RDFFile> {
 
     private static final Logger LOGGER = LogManager.getLogger(OutputOption.class);
-    private static final String DEFAULT_DIR = "output";
-    private static final String DIR_DESC = "RDF output directory. (default: ${DEFAULT-VALUE})";
-    private static final String DEFAULT_FORMAT = "NTRIPLES";
-    private static final String FORMAT_DESC = "RDF output file format. (default: ${DEFAULT-VALUE})";
-    private static final String DEFAULT_FILENAME = "rdfOutput.nt";
-    private static final String FILENAME_DESC = "RDF output file name. (default: ${DEFAULT-VALUE})";
-
     private final RDFFileFactory factory;
-    @Option(names = {"-d", "--dir"}, defaultValue = DEFAULT_DIR, description = DIR_DESC)
-    private File path;
-    @Option(names = {"-n", "--filename"}, defaultValue = DEFAULT_FILENAME, description = FILENAME_DESC)
-    private String filename;
-    @Option(names = {"-f", "--format"}, defaultValue = DEFAULT_FORMAT, description = FORMAT_DESC)
-    private RDFFileFormat format;
-    @Spec private CommandSpec spec;
+    @ArgGroup
+    private final FileSystem fileSystem = new FileSystem();
+    @Spec
+    private CommandSpec spec;
 
     public OutputOption() {
         this.factory = new RDFFileFactory();
@@ -56,13 +44,13 @@ public class OutputOption implements Callable<RDFFile> {
         CommandLine cmd = spec.commandLine();
         cmd.setCaseInsensitiveEnumValuesAllowed(false);
         try {
-            Path directoryPath = path.toPath();
-            Path filePath = FileResolver.resolveFilename(directoryPath, filename);
-            RDFFile file = factory.createFile(filePath, format.getFormat());
+            Path directoryPath = fileSystem.path.toPath();
+            Path filePath = FileResolver.resolveFilename(directoryPath, fileSystem.filename);
+            RDFFile file = factory.createFile(filePath, fileSystem.format.getFormat());
             FileResolver.prepareDir(directoryPath);
             return file;
         } catch (IOException ex) {
-            String msg = "I/O error: Failed to create directory path: " + path;
+            String msg = "I/O error: Failed to create directory path: " + fileSystem.path.toPath();
             cmd.getOut().println(msg);
             LOGGER.fatal(msg, ex);
             throw new ExecutionException(cmd, msg, ex);
@@ -72,5 +60,35 @@ public class OutputOption implements Callable<RDFFile> {
             cmd.getOut().println(msg);
             throw new ExecutionException(cmd, msg, ex);
         }
+    }
+
+    static class FileSystem {
+        private static final String DEFAULT_DIR = "output";
+        private static final String DIR_DESC = "RDF output directory. (default: ${DEFAULT-VALUE})";
+        private static final String DEFAULT_FORMAT = "NTRIPLES";
+        private static final String FORMAT_DESC = "RDF output file format. (default: ${DEFAULT-VALUE})";
+        private static final String DEFAULT_FILENAME = "rdfOutput.nt";
+        private static final String FILENAME_DESC = "RDF output file name. (default: ${DEFAULT-VALUE})";
+        @Option(names = {"-d", "--dir"}, defaultValue = DEFAULT_DIR, description = DIR_DESC)
+        private File path;
+        @Option(names = {"-n", "--filename"}, defaultValue = DEFAULT_FILENAME, description = FILENAME_DESC)
+        private String filename;
+        @Option(names = {"-f", "--format"}, defaultValue = DEFAULT_FORMAT, description = FORMAT_DESC)
+        private RDFFileFormat format;
+    }
+
+    static class FusekiTDB {
+        private static final String HOST_DESC = "Fuseki server host name";
+        private static final String PORT_DESC = "Fuseki server port";
+        private static final String BASE_DESC = "RDF dataset base name";
+        private static final String GRAPH_DESC = "RDF graph name";
+        @Option(names = {"-h", "--host"}, required = true, description = HOST_DESC)
+        private InetAddress host;
+        @Option(names = {"-p", "--port"}, required = true, description = PORT_DESC)
+        private int port;
+        @Option(names = {"-b", "--baseName"}, description = BASE_DESC)
+        private String baseName;
+        @Option(names = {"-g", "--graphName"}, description = GRAPH_DESC)
+        private String graphName;
     }
 }
