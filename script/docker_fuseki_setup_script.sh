cff1559bf20d0a7bd18fa80631e9017869984959
jiefenn8 <jiefenn8@outlook.com>
2020-12-17 01:04:49 +1300

    Fix Fuseki docker setup script
    [skip ci]

--- script/docker_fuseki_setup_script.sh
+++ script/docker_fuseki_setup_script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #Script to setup Jena Fuseki database with a docker image
 echo "Starting fuseki docker setup script"
-docker run -p 3030:3030 --name fuseki1 stain/jena-fuseki --update --mem /ds &&
+docker run -d -e FUSEKI_DATASET_1=ds -p 3030:3030 --name fuseki01 stain/jena-fuseki
 wait
 echo "Finished fuseki docker setup script"
  
